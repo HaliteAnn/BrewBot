70700e9110edb21e47912cda6d025150b74ed14a
Halite <halite.ann@gmail.com>
2017-02-18 12:20:51 -0500

    Created project

---
+++ src/main/java/personal/halite/brewbot/BrewBot.java
@@ -0,0 +1,12 @@
+package personal.halite.brewbot;
+
+/**
+ * Created by Halite on 2/18/2017.
+ */
+public class BrewBot {
+
+    public static void main(String[] args){
+        System.out.println("BrewBot push test.");
+    }
+
+}
