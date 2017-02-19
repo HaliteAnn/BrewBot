545829287764fb73791419980adf6c0dde74c74e
Halite <halite.ann@gmail.com>
2017-02-18 22:45:19 -0500

    Minor fixes to terminal output and versioning

--- src/main/java/personal/halite/brewbot/BrewBot.java
+++ src/main/java/personal/halite/brewbot/BrewBot.java
@@ -235,7 +235,7 @@ public class BrewBot {
 
     public static void main(String[] args){
 
-        System.out.println("BrewBot push test.");
+        System.out.println("Starting BrewBot.");
 
         BrewBot bot = new BrewBot();
 
